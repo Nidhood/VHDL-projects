f56ffcb35dd8bf4e030e309bcf18660324ea9647
Jero2806 <76265129+Jero2806@users.noreply.github.com>
2025-02-24 20:21:17 -0500

    Update fpga01.vhd

--- projects/project_lab03/pnt2/bin_to_sseg.vhd
+++ projects/project_lab03/pnt2/bin_to_sseg.vhd
@@ -1,32 +1,17 @@
 LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_1164.all;
 
-ENTITY bin_to_sseg IS
-    PORT (
-        bin : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-        sseg : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)
-    );
-END ENTITY bin_to_sseg;
+ENTITY fpga01 IS
+    PORT( bin  : IN  STD_LOGIC_VECTOR(1 DOWNTO 0);
+          led : OUT STD_LOGIC
+        );
+END ENTITY fpga01;
 
-ARCHITECTURE behaviour OF bin_to_sseg IS
+ARCHITECTURE behaviour OF fpga01 IS
 BEGIN
     WITH bin SELECT
-        sseg <=
-        "1000000" WHEN "0000",
-        "1111001" WHEN "0001",
-        "0100100" WHEN "0010",
-        "0110000" WHEN "0011",
-        "0011001" WHEN "0100",
-        "0010010" WHEN "0101",
-        "0000010" WHEN "0110",
-        "1111000" WHEN "0111",
-        "0000000" WHEN "1000",
-        "0010000" WHEN "1001",
-        "0001000" WHEN "1010", -- A
-        "0000011" WHEN "1011", -- B
-        "1000110" WHEN "1100", -- C
-        "0100001" WHEN "1101", -- D
-        "0000110" WHEN "1110", -- E
-        "0001110" WHEN OTHERS; -- F
-
+        led <=
+		      '1' WHEN "01",
+				'0' WHEN OTHERS;
+			
 END behaviour;
