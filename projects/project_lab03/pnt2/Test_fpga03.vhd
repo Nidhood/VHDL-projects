c84e372807f8de93cb1dc9d0f28d775efe5cc035
Nidhood <ivan.oroz34@gmial.com>
2025-02-26 16:16:13 -0500

    🚀 RELEASE:
    Finally finished lab3

--- projects/project_lab03/pnt2/Test_fpga03.vhd
+++ projects/project_lab03/pnt2/Test_fpga03.vhd
@@ -5,23 +5,25 @@ ENTITY Test_fpga03 IS
 END Test_fpga03;
 
 ARCHITECTURE Test_fpga03Arch OF Test_fpga03 IS
-    SIGNAL bin : STD_LOGIC_VECTOR(2 DOWNTO 0) := "000";
-    SIGNAL led : STD_LOGIC_VECTOR(1 DOWNTO 0);
-    
+    SIGNAL bin : STD_LOGIC_VECTOR(10 DOWNTO 0);
+    SIGNAL led : STD_LOGIC_VECTOR(9 DOWNTO 0);
+
 BEGIN
     DUT : ENTITY WORK.fpga03
-        PORT MAP (
+        PORT MAP(
             bin => bin,
             led => led
         );
 
-    bin <= "000" AFTER 20 ns, 
-          "001" AFTER 40 ns,  
-          "010" AFTER 60 ns,  
-          "011" AFTER 80 ns,  
-          "100" AFTER 100 ns, 
-          "101" AFTER 120 ns, 
-          "110" AFTER 140 ns, 
-          "111" AFTER 160 ns; 
-			 
+    bin <=
+        "10000000001" AFTER 0 ns,
+        "00000000010" AFTER 20 ns,
+        "10000000100" AFTER 40 ns,
+        "00000001000" AFTER 60 ns,
+        "10000010000" AFTER 80 ns,
+        "00000100000" AFTER 100 ns,
+        "10001000000" AFTER 120 ns,
+        "00010000000" AFTER 140 ns,
+        "10100000000" AFTER 160 ns,
+        "01000000000" AFTER 180 ns;
 END Test_fpga03Arch;
