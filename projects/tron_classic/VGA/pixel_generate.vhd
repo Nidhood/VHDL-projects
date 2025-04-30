c5ac699607654a40c7944e1d90f43890c7ee0047
Nidhood <ivan.oroz34@gmial.com>
2025-04-30 16:37:30 -0500

    🚀 RELEASE: Añadir paquete SquarePkg y mejorar la lógica de generación de píxeles en PixelGenerate

--- projects/tron_classic/VGA/pixel_generate.vhd
+++ projects/tron_classic/VGA/pixel_generate.vhd
@@ -1,31 +1,69 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
-
+USE IEEE.NUMERIC_STD.ALL;
 USE WORK.BasicPackage.ALL;
 USE WORK.VgaPackage.ALL;
+USE WORK.SquarePkg.ALL;
 
 ENTITY PixelGenerate IS
-
+    GENERIC (
+        SQ_X0 : INTEGER := 400; -- X inicial del sprite
+        SQ_Y0 : INTEGER := 300 -- Y inicial del sprite
+    );
     PORT (
         PosX : IN uint11;
         PosY : IN uint11;
         VideoOn : IN uint01;
         RGB : OUT ColorT
     );
-
-END PixelGenerate;
+END ENTITY;
 
 ARCHITECTURE MainArch OF PixelGenerate IS
-    SIGNAL Color : uint08;
-
+    SIGNAL xi, yi : INTEGER;
 BEGIN
 
-    RGB.R <= Color;
-    RGB.G <= Color;
-    RGB.B <= Color;
+    -- Convertir PosX/PosY a enteros
+    xi <= to_integer(unsigned(PosX));
+    yi <= to_integer(unsigned(PosY));
 
-    WITH VideoOn SELECT
-        Color <= "11111111" WHEN '1',
-        "00000000" WHEN OTHERS;
+    PROCESS (xi, yi, VideoOn)
+        -- Variables locales combinacionales
+        VARIABLE idx_v : INTEGER RANGE 0 TO SQSZ - 1;
+        VARIABLE px_v : STD_LOGIC_VECTOR(7 DOWNTO 0);
+    BEGIN
+        IF VideoOn = '1' THEN
+            -- ¿Dentro del área del sprite?
+            IF xi >= SQ_X0 AND xi < SQ_X0 + SQW
+                AND yi >= SQ_Y0 AND yi < SQ_Y0 + SQH THEN
 
-END MainArch;
+                -- Calcular índice en el array 1D
+                idx_v := (yi - SQ_Y0) * SQW + (xi - SQ_X0);
+                px_v := SquareData(idx_v);
+
+                -- Transparencia: x"FF" muestra fondo (azul); otro valor pinta sprite (negro)
+                IF px_v = x"FF" THEN
+                    RGB.R <= (OTHERS => '0');
+                    RGB.G <= (OTHERS => '0');
+                    RGB.B <= (OTHERS => '1');
+                ELSE
+                    RGB.R <= (OTHERS => '0');
+                    RGB.G <= (OTHERS => '0');
+                    RGB.B <= (OTHERS => '0');
+                END IF;
+
+            ELSE
+                -- Fondo azul
+                RGB.R <= (OTHERS => '0');
+                RGB.G <= (OTHERS => '0');
+                RGB.B <= (OTHERS => '1');
+            END IF;
+
+        ELSE
+            -- Fuera de video: negro
+            RGB.R <= (OTHERS => '0');
+            RGB.G <= (OTHERS => '0');
+            RGB.B <= (OTHERS => '0');
+        END IF;
+    END PROCESS;
+
+END ARCHITECTURE;
